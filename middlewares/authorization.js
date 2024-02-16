238b7dd0be70b284deb4e794e9b6d3228ab0b27e
Mohitxroxx <mohitsinghroxx@gmail.com>
2024-02-16 15:52:58 +0530

    fixed auth bud

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -2,11 +2,12 @@ const roles = ['admin', 'member']
 
 const authorization = (role) => {
     return (req, res, next) => {
-        if (!req.user) {
-            return res.status(403).json({
-                message: 'User not logged in'
-            })
-        }
+        console.log(req.body)
+        // if (!req.user) {
+        //     return res.status(403).json({
+        //         message: 'User not logged in'
+        //     })
+        // }
         if (roles.indexOf(req.user.role) > roles.indexOf(role)) {
             return res.status(403).json({
                 message: 'User isnt allowed to access this service'
