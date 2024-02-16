6926b0b62c9bfcd2cefefbd4028deeffcb01c809
Mohitxroxx <mohitsinghroxx@gmail.com>
2024-02-16 14:21:33 +0530

    auth fix

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -1,15 +1,15 @@
-const roles = ['admin', 'member', 'user']
+const roles = ['admin', 'member']
 
 const authorization = (role) => {
     return (req, res, next) => {
         if (!req.user) {
             return res.status(403).json({
-                message: 'forbidden'
+                message: 'User not logged in'
             })
         }
         if (roles.indexOf(req.user.role) > roles.indexOf(role)) {
             return res.status(403).json({
-                message: 'forbidden'
+                message: 'User isnt allowed to access this service'
             })
         }
         next()
