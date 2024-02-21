97e26aee763e4d3a766fa51d0d270c5acbdf1458
Mohitxroxx <mohitsinghroxx@gmail.com>
2024-02-21 15:19:26 +0530

    edited package.json

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ dotenv.config()
 const app = express()
 
 app.use(cors({
-    origin: '*',
+    origin: 'http://localhost:3000',
     credentials: true
 }))
 app.use(express.urlencoded({ extended: false }))
